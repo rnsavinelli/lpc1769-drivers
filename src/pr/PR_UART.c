e7c68b97ebe916137f7222c33e1e9e0e46175194
R. Nicolás Savinelli <rsavinelli@est.frba.utn.edu.ar>
2019-12-04 16:07:23 -0300

    Update PR_UART.c

--- src/pr/PR_UART.c
+++ src/pr/PR_UART.c
@@ -8,12 +8,14 @@
 #include "DR_UART.h"
 #include "PR_UART.h"
 
-// need a flag to tell if the uart was initialized
-
 uint8_t SendData(uint8_t uart_number, const int8_t * data, uint8_t data_length) {
 	for(uint8_t i = 0; i < data_length; i++) {
 		UARTn_PushTx(uart_number, data[i]);
 	}
 
 	return 0;
+}
+
+uint16_t ReceiveData(uint8_t uart_number) {
+	return UARTn_PopRx(uart_number);
 }
